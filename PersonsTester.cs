737c989c36b763e0141bc2812ce5005a0b64b84a
FelixSD <felix_98@outlook.de>
2019-12-11 17:18:35 +0100

    L07 todos Iterator

---
+++ PersonsTester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace Enumerables
+{
+    class PersonsTester
+    {
+        public static void Main(string[] args)
+        {
+            string[] stringArray = new string[]{"test1", "test2", "test3", "test4"};
+            var test = stringArray.GetEnumerator();
+            while(test.MoveNext())
+            {
+                Console.WriteLine(test.Current);
+            }
+        }
+    }
+}
